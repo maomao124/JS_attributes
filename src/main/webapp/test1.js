f4a5d5345ef3e5c02ce408288347a98b25fea28a
毛 <1296193245@qq.com>
2022-03-08 12:55:24 +0800

    JS_attributes

---
+++ src/main/webapp/test1.js
@@ -0,0 +1,25 @@
+/**
+ * Project name(项目名称)：JS_attributes
+ * File name(文件名): test1
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/3/8
+ * Time(创建时间)： 12:47
+ * Version(版本): 1.0
+ * Description(描述)： 无
+ */
+
+
+
+// 属性 / 方法	    描述
+// attributes.isId	如果属性是 ID 类型，则返回 true，否则返回 false
+// attributes.name	返回属性名称
+// attributes.value	设置或者返回属性的值
+// attributes.specified	如果定义了指定属性，则返回 true，否则返回 false
+// nodemap.getNamedItem()	从节点列表中返回的指定属性节点
+// nodemap.item()	返回节点列表中处于指定索引号的节点
+// nodemap.length	返回节点列表的节点数目
+// nodemap.removeNamedItem()	删除指定属性节点
+// nodemap.setNamedItem()	设置指定属性节点（通过名称）
+
